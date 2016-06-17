57f1fb0c30b470ff7208944cb8b3bae418178f77
Go2 <go2@GOs-iMac.local>
2016-06-17 14:37:02 -0500

    comentario

---
+++ chat/main.go
@@ -0,0 +1,45 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"text/template"
+)
+import "os"
+
+var homeTemplate = template.Must(template.ParseFiles("home.html"))
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	homeTemplate.Execute(w, r.Host)
+}
+
+func main() {
+
+	puerto := os.Getenv("PORT")
+	if puerto == "" {
+    puerto = "8081"
+	}
+	var addr = flag.String("addr", ":"+puerto, "http service address")
+	flag.Parse()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", serveWs)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
